ac1be55bf7cb236651e4cbee64a5636f3f063fd9
MnStan <mmaks97@gmail.com>
2023-11-23 15:56:10 +0100

    Added temporary page to get first user

--- app/page.tsx
+++ app/page.tsx
@@ -3,3 +3,8 @@ import React, {useState, useEffect} from 'react';
 import { api } from '@/lib/api';
 import { PrintEndpoint } from '@/components';
 import axios from 'axios';
+
+
+export default function HomePage() {
+  return <h1>HomePage</h1>
+}
--- components/PrintEndpoint.tsx
+++ components/PrintEndpoint.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import {User} from '@/lib/data'
+import { UserInfo } from '@/lib/data'
 
-const PrintEndpoint = ({id, email}: User) => {
+const PrintEndpoint = ({user_info_id, name, surname, phone, address}: UserInfo) => {
     return (
       <div className="m-5 p-5 bg-gray-300 w-auto lg:w-1/2 flex justify-center" key={1}>
-        <h2>id: {id}</h2>
-        <h2>title: {email}</h2>
+        <h2>id: {user_info_id}</h2>
+        <h2>name: {name}</h2>
+        <h2>surname: {surname}</h2>
       </div>
     )
   }
