b181c0a7bf62f75e7a45e04c90f7c7be53e1bbea
MnStan <mmaks97@gmail.com>
2024-01-31 23:59:06 +0100

    Added api for getting all cars from database, getting images from server and added car card component for displaying cars from database. Removed /public folder from git and added it to .gitignore

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,41 @@
 "use client"
 
 import React, {useState, useEffect} from 'react';
-import { Button } from '@/components/ui/button';
+import axios from 'axios';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import SearchBar from '@/components/ui/search';
+import CarComponent from '@/components/CarCard';
+
+interface Car {
+  name: string;
+  photos: { photo_id: string, photo_url: string }[];
+}
 
 export default function HomePage() {
-  
+  const [cars, setCars] = useState<Car[]>([]);
+
+  useEffect(() => {
+    axios.get('/api/getAllCars')
+      .then(response => {
+        if (response.data.status === 'success') {
+          setCars(response.data.data);
+        }
+      })
+      .catch(error => {
+        console.error('Błąd podczas pobierania danych', error);
+      });
+  }, []);
+
   return (
     <>
       <Header />
-      <main>
-        <SearchBar />
-      </main>
+      <SearchBar />
+      {cars.map(car => (
+        car.photos.map(photo => (
+          <CarComponent key={photo.photo_id} apiEndpoint={`/api/image/${photo.photo_url}/${photo.photo_id}`} label={car.name}/>
+        ))
+      ))}
       <Footer />
     </>
   )
